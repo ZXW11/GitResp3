6b753fca7e899ab339b0e5c9f386d1fefc5212ac
zhuangxw <2065097422@qq.com>
2022-12-20 10:48:40 +0800

    在Person类中增加了age属性

---
+++ TestModule01/src/com/msb/Person.java
@@ -0,0 +1,11 @@
+package com.msb;
+
+/**
+ * @Auther: zhuangxw
+ * @Date: 2022/12/20 - 12 - 20 - 10:47
+ * @Description: com.msb
+ * @version: 1.0
+ */
+public class Person {
+    private int age;
+}
